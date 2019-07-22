f28267472d39154fd6ae02e6acf6ce1bf5855a83
Bikash <bikash@stridesolution.com.au>
2019-07-22 17:27:18 +1000

    added new controller and user name

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,22 +2,40 @@
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    You are logged in!
-                </div>
+    <div class="row">
+        <div class="col-3 p-5">
+            <img src="../images/freecodecamplogo.jpg" style="height:100px;" class="rounded-circle">
+        </div>
+        <div class="col-9 pt-5">
+            <div>
+                <h1>freecodecamp</h1>
             </div>
+            <div class="d-flex">
+                <div class="pr-5"><strong>153 </strong>posts</div>
+                <div class="pr-5"><strong>23k </strong>followers</div>              
+                <div class="pr-5"><strong>212 </strong>following</div>             
+            </div>
+
+            <div class="pt-4 font-weight-bold"><strong>freeCodeCamp.org</strong></div>
+            <div>We're a global community of millions of people learning to code together. We're an open source, donor-supported, 501(c)(3) nonprofit.</div>
+            <div><a href="www.freecodecamp.org"><strong>www.freecodecamp.org</strong></a></div>
+
         </div>
     </div>
+
+   <div class="row pt-5">
+       <div class="col-4">
+           <img src="https://instagram.fmel5-1.fna.fbcdn.net/vp/594b9521aba3b942867056cdc4f9625f/5DE5B9A9/t51.2885-15/sh0.08/e35/c1.0.748.748a/s640x640/65142981_753889638347402_2885440414377677851_n.jpg?_nc_ht=instagram.fmel5-1.fna.fbcdn.net" class="w-100">
+       </div>
+           <div class="col-4">
+           <img src="https://instagram.fmel5-1.fna.fbcdn.net/vp/b35bdca93b9771ad2452ebf0d207a3be/5DB57AF4/t51.2885-15/sh0.08/e35/c0.107.924.924a/s640x640/57251409_126311265211024_8203617265278055615_n.jpg?_nc_ht=instagram.fmel5-1.fna.fbcdn.net" class="w-100">
+
+           </div>
+       <div class="col-4">
+            <img src="https://instagram.fmel5-1.fna.fbcdn.net/vp/8ca3a7b3acb90fb334c2999be6af14fe/5DADB5D8/t51.2885-15/sh0.08/e35/s640x640/55764841_116118912682605_8460689535092889415_n.jpg?_nc_ht=instagram.fmel5-1.fna.fbcdn.net" class="w-100">
+
+       </div>
+   </div>
 </div>
+
 @endsection
